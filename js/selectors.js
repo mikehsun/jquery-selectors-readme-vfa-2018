086506759b009e366336c624c7c111182d2494a1
Michael Hsun <mikehsun@gmail.com>
2018-06-20 10:08:47 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,14 +1,14 @@
 // declare your functions here...
 function paragraphSelector(){
-  return $('p')
+  return $('p');
 }
 
 function lastImageSelector(){
-  $('img:last')
+  return $('img:last');
 }
 
 function ninjaBabySelector(){
-  $('#baby-ninja')
+  return $('#baby-ninja');
 }
 
 function divSelector(){
