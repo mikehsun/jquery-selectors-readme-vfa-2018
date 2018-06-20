5fdaee3e658f483d45289693360ef47f23f45486
Michael Hsun <mikehsun@gmail.com>
2018-06-20 10:06:14 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,21 @@
 // declare your functions here...
+function paragraphSelector(){
+  $('p')
+}
+
+function lastImageSelector(){
+  $('img:last')
+}
+
+function ninjaBabySelector(){
+  $('#baby-ninja')
+}
+
+function divSelector(){
+  $('.pics')
+}
+
+function firstListItem(){
+  $('parent-tag child-tag:first-child')
+  $('div img:first:child')
+}
